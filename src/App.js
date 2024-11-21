15e1a251d5d794a06b646e322d7aaa76a8ab7413
networkSorcerer <gigattfyhig@gmail.com>
2024-11-21 15:55:48 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
   return (
-    <>
-      <p>App</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
-  return <></>;
+  return (
+    <>
+      <h1>여기는 About 페이지입니다.</h1>
+      <p>소개 페이지 데스</p>
+      <Link to="/">홈으로 이동</Link>
+    </>
+  );
 };
+export default About;
